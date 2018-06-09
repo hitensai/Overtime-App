405af1bcb864933d0c981fff88dd15a382a41a3e
hiten <hitensai279@gmail.com>
2018-06-09 14:32:02 -0500

    used resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-#This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_172810) do
+ActiveRecord::Schema.define(version: 2018_06_09_184900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.date "date"
+    t.text "rationale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
