370ae2c85897a909dd001a9c46be932941663329
hiten <hitensai279@gmail.com>
2018-06-09 15:59:48 -0500

    refactored code after bdd CRUD

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,15 +2,30 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
-   it 'can be succesfully created' do
-   visit posts_path
-   expect(page.status_code).to eq(200)
-   end
-   it 'cannot be empty' do
-   visit posts_path
-   expect(page).to have_content(/posts/)
-   end
-  end 
+       it 'can be succesfully created' do
+          visit posts_path
+          expect(page.status_code).to eq(200)
+        end
+       it 'cannot be empty' do
+          visit posts_path
+           expect(page).to have_content(/posts/)
+        end
+  end
+  describe 'creation ' do
+  	  before do 
+  	  	visit new_post_path
+  	  end	
+      it 'has a new form' do
+   	       expect(page.status_code).to eq(200)
+        end
 
+       it 'can be created' do
+   	       fill_in 'post[date]', with: Date.today
+   	        fill_in 'post[rationale]', with: "some rationale"
+
+   	        click_on "save"
+   	        expect(page).to have_content(/some rationale/)
+        end
+   end
 
 end
