eb41fc3934a73c70be2448a1c3210fb6a3690264
hiten <hitensai279@gmail.com>
2018-08-17 16:54:08 -0500

    rspec error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user=User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "hiten", last_name: "sai")
+@user=User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "hiten", last_name: "sai")
 puts "1 user created"
 
 100.times do |post|
